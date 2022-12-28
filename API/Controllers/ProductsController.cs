bf72c6ecddb169cfd986e028bd47e3ac2d1a7f05
Nicholas Harding <40708936+sir306@users.noreply.github.com>
2022-12-29 09:22:35 +1300

    Created http gets for brands and types, and tweaked the gets for products so now brands and types show up on requests

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -30,5 +30,17 @@ namespace API.Controllers
         {
             return await _repo.GetProductByIdAsync(id);
         }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<ProductBrand>> GetProductBrands()
+        {
+            return Ok(await _repo.GetProductBrandsAsync());
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<ProductType>> GetProductTypes()
+        {
+            return Ok(await _repo.GetProductTypesAsync());
+        }
     }
 }
