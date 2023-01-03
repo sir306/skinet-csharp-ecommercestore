8bce6e7663bf02695d5e8b99f29029468e38b17b
Nicholas Harding <40708936+sir306@users.noreply.github.com>
2023-01-03 14:17:16 +1300

    created some typescript interfaces to get the products being returned

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  // properties
   title = 'SkiNet';
+  products: IProduct[];
+
+  // constructor
+  constructor(private http: HttpClient) { }
+
+  // lifestyle hooks
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe({
+      next: (response: IPagination) => this.products = response.data,
+      error: (e) => console.error(e),
+      complete: () => console.info('complete', this.products)
+    })
+  }
 }
